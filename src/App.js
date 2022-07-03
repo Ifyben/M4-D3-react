44236413266c4b1610035d79da677ab9b453f480
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-07-04 00:05:17 +0200

    I created a component called MyBadge which receives a text and a color as props. This component should render a Badge react-bootstrap component with those properties.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import WarningSign from './components/WarningSign';
+import MyBadge from './components/MyBadge';
 
 function App() {
   return (
     <div className='App'> 
     <header className='App-header'>
     <WarningSign text="Watch Out!" />
+    <MyBadge text="NEW!!" color="info" />
     </header>
     </div> 
   );
