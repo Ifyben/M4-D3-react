64762ab844bd4c7f5cdd651b311a9a921e29e987
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-07-04 01:53:05 +0200

    I created  a BookList component. This component receives by props a list of books and displays them using the SingleBook component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import WarningSign from './components/WarningSign';
 import MyBadge from './components/MyBadge';
 import SingleBook from './components/SingleBook';
+import BookList from './components/BookList';
 import fantasyBooks from './fantasyBooks.json'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <WarningSign text="Watch Out!" />
     <MyBadge text="NEW!!" color="info" />
     <SingleBook book={fantasyBooks[0]} />
+    <BookList books={fantasyBooks} />
     </header>
     </div> 
   );
