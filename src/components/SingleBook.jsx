f5e1718fe4281b48dee1cf5da2fdb2c1591187f1
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-07-04 17:47:42 +0200

    Clicking on a SingleBook should toggle its 'selected' state property . If the 'selected' property is true, the SingleBook should have some styling that reflects that state change, visually.

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -8,7 +8,10 @@ class SingleBook extends React.Component {
 
     render() {
         return (
-            <Card>
+            <Card
+                onClick={() => this.setState({ selected: !this.state.selected })}
+                style={{ border: this.state.selected ? '3px solid red' : 'none' }}
+            >
                 <Card.Img variant="top" src={this.props.book.img} />
                 <Card.Body>
                     <Card.Title style={{color: 'black' }}>{this.props.book.title}</Card.Title>
