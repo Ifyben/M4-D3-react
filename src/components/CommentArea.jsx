fe636f9105ae9b44ee92c0e70fca7a4365c42786
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-07-11 01:26:13 +0200

    I created a Loading component. This Loading component appears on the page when a remote request is ongoing.

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,10 +1,12 @@
 import { Component } from "react";
 import CommentsList from "./CommentsList";
 import AddComment from "./AddComment";
+import Loading from "./Loading";
 
 class CommentArea extends Component {
     state = {
-        comments: [] //comments will go here
+        comments: [], //comments will go here
+        isLoading: true
     }
 
 
@@ -17,24 +19,31 @@ class CommentArea extends Component {
                     Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMWQwNjRiYjUzZDAwMTViMTllY2YiLCJpYXQiOjE2NTcwMzY3NzUsImV4cCI6MTY1ODI0NjM3NX0.Fomv5CitgDSLTApWJflGo_wLBuL6VKT7_dpsr82Z0gU"
                 }
             })
-            
-            
-            let comments = await response.json() 
-            console.log(comments)
-            this.setState({ comments: comments}) 
+            console.log(response)
+            if(response.ok) {
+                let comments = await response.json() 
+                this.setState({ comments: comments, isLoading: false})
+            } else {
+                console.log('error')
+                this.setState({ isLoading: false })
+            }
+             
         } catch (error) {
             console.log(error) 
+            this.setState({ isLoading: false })
         }
     }
 
     render() {
         return (
             <div>
+                 {this.state.isLoading && <Loading />}
                 <AddComment asin={this.props.asin} />
-                <CommentsList commentsToShow={this.state.comments}/> 
+                <CommentsList commentsToShow={this.state.comments} /> 
             </div>
         )
     }
 }
 
 export default CommentArea
+
