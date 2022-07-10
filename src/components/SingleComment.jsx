516b4832018b098a3a91d8c69be46fc6d4d9bb87
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-07-10 19:09:20 +0200

    I added to each 'Comment', a button for DELETING it. I created a request towards the api.

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,9 +1,29 @@
-import { ListGroup } from "react-bootstrap"
+import { Button, ListGroup } from "react-bootstrap"
 
+const deleteComment = async (asin) => {
+    try {
+        let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + asin, {
+            method: 'DELETE', 
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMWQwNjRiYjUzZDAwMTViMTllY2YiLCJpYXQiOjE2NTcwMzY3NzUsImV4cCI6MTY1ODI0NjM3NX0.Fomv5CitgDSLTApWJflGo_wLBuL6VKT7_dpsr82Z0gU"
+            }
+        }) 
+        if(response.ok) {
+            alert('comment deleted!') 
+        } else {
+            alert('comment NOT deleted!')
+        }
+    } catch (error) {
+        alert('comment NOT deleted!') 
+    }
+}
 
 const SingleComment = ({ comment }) => (
     
-      <ListGroup.Item>{comment.comment}</ListGroup.Item>
+    <ListGroup.Item>
+        {comment.comment}
+        <Button variant="danger" className="ml-2" onClick={() => deleteComment(comment._id)}>D</Button>
+    </ListGroup.Item>
      
 )
 
