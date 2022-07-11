3f6c44dc768cc621fc40ebd4f941d2d41341969c
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-07-11 19:31:30 +0200

    commit

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -14,8 +14,9 @@ class SingleBook extends React.Component {
                 onClick={() => this.setState({ selected: !this.state.selected })}
                 style={{ border: this.state.selected ? '3px solid red' : 'none' }}
             >
-                <Card.Img variant="top" src={this.props.book.img} style={{ width: '260px', height: '400px' }} />
-                <Card.Body>
+                <Card.Img variant="top" src={this.props.book.img} style={{ height: '400px' }} />
+                
+                <Card.Body style={{}}>
                     <Card.Title style={{color: 'black', width: 'auto', height: '100px' }}>{this.props.book.title}</Card.Title>
                 </Card.Body>
             </Card>
