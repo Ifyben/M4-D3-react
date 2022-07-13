2ad73719850bd7e80469a07926d11e901b4b670a
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-07-13 21:11:17 +0200

    commit
    When the submit button is clicked, it displays a recap of the information submitted.

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -39,40 +39,61 @@ class Registration extends Component {
         return isClickable
     }
     
+    handleRegistration = (e) => {
+        e.preventDefault() 
+        this.setState({
+            showComplete: true    
+        })
+    }
+
     render() {
         return (
             <>
-            <h1>PLEASE FILL IN THE FORM</h1>
-            <Form>
-            <Form.Group>
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control type="text" placeholder="Insert name" required onChange={(e) => this.handleInput('name', e.target.value)} value={this.state.registration.name} />
-                </Form.Group>
+                {
+                    this.state.showComplete ? (
+                        <>
+                        <div>WELL DONE, THE FORM HAS BEEN SUBMITTED SUCCESSFULLY. HERE IS YOUR RECAP:</div>
+                        <p>{this.state.registration.name}</p>
+                        <p>{this.state.registration.surname}</p>
+                        <p>{this.state.registration.email}</p>
+                        </>
+                    ) : (
+                        <>
 
-                <Form.Group>
-                  <Form.Label>Surname</Form.Label>
-                  <Form.Control type="text" placeholder="Insert surname" required onChange={(e) => this.handleInput('surname', e.target.value)} value={this.state.registration.surname} />
-                </Form.Group>
+                            <h1>PLEASE FILL IN THE FORM</h1>
+                            <Form onSubmit={this.handleRegistration}>
+                            <Form.Group>
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="text" placeholder="Insert name" required onChange={(e) => this.handleInput('name', e.target.value)} value={this.state.registration.name} />
+                                </Form.Group>
+
+                                <Form.Group>
+                                <Form.Label>Surname</Form.Label>
+                                <Form.Control type="text" placeholder="Insert surname" required onChange={(e) => this.handleInput('surname', e.target.value)} value={this.state.registration.surname} />
+                                </Form.Group>
 
 
-                <Form.Group>
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" required onChange={(e) => this.handleInput('email', e.target.value)} value={this.state.registration.email} />
-                </Form.Group>
+                                <Form.Group>
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" required onChange={(e) => this.handleInput('email', e.target.value)} value={this.state.registration.email} />
+                                </Form.Group>
 
-                <Form.Group>
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" required onChange={(e) => this.handleInput('password', e.target.value)} value={this.state.registration.password} />
-                </Form.Group>
+                                <Form.Group>
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" required onChange={(e) => this.handleInput('password', e.target.value)} value={this.state.registration.password} />
+                                </Form.Group>
 
-                <Form.Group>
-                  <Form.Label>Repeat Password</Form.Label>
-                  <Form.Control type="password" placeholder="Repeat Password" required onChange={(e) => this.handleInput('confirmPassword', e.target.value)} value={this.state.registration.confirmPassword} />
-                </Form.Group>
-                <Button variant="primary" type="submit" disabled={!this.isButtonClickable()} >
-                  Submit
-                </Button>
-            </Form>
+                                <Form.Group>
+                                <Form.Label>Repeat Password</Form.Label>
+                                <Form.Control type="password" placeholder="Repeat Password" required onChange={(e) => this.handleInput('confirmPassword', e.target.value)} value={this.state.registration.confirmPassword} />
+                                </Form.Group>
+                                <Button variant="primary" type="submit" disabled={!this.isButtonClickable()} >
+                                 Submit
+                                </Button>
+                            </Form>
+                        </>
+                    )
+                }
             </>
         )
     }
